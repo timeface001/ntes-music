db3c0862c4a36f43064a40eba0850b1b1910c1f1
冯松 <hardly.a.challenge.for.me@gmail.com>
2017-08-01 17:52:08 +0800

    首次提交

---
+++ src/java/com/fs/dreams/NtesMusicApplication.java
@@ -0,0 +1,12 @@
+package com.fs.dreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtesMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtesMusicApplication.class, args);
+	}
+}
---
+++ src/java/com/fs/dreams/mapper/ArtistMapperExt.java
@@ -0,0 +1,15 @@
+package com.fs.dreams.mapper;/**
+ * Created by fengsong on 2017/8/1.
+ */
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author fengsong
+ * @description:一句话描述下类的功能
+ * @time 2017-08-01 16:56
+ **/
+@Mapper
+public interface ArtistMapperExt extends ArtistMapper{
+
+}
